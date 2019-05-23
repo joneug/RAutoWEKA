2443dded6a44a4c5f66e3ceee82e2140977e8a64
Jonathan Neugebauer <jonathan.n@uni-muenster.de>
2019-05-23 23:14:16 +0200

    Fix test

--- tests/testthat/test-classifier.R
+++ tests/testthat/test-classifier.R
@@ -1,7 +1,5 @@
 context("test-classifier")
 
 testthat::test_that("buildAutoWekaClassifier works", {
-  download.file("https://storm.cis.fordham.edu/~gweiss/data-mining/weka-data/iris.arff", "iris.arff")
-  buildAutoWekaClassifier("iris.arff", timeLimit = 1, noCrossValidation = TRUE)
-  file.remove("iris.arff")
+  buildAutoWekaClassifier(iris, timeLimit = 1)
 })
