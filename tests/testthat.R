07412b9e9b5c95521e9892a94a23128873b821e4
Jonathan Neugebauer <jonathan.n@uni-muenster.de>
2019-06-24 22:43:56 +0200

    Refactor code, add tests and documentation

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,17 @@
-library(testthat)
+# Make shure testthat is installed
+if(!require(testthat)){
+  install.packages("testthat")
+  library(testthat)
+}
+
+# Make shure mlbench is installed
+if(!require(mlbench)){
+  install.packages("mlbench")
+  library(mlbench)
+}
+
+# Load packge
 library(RAutoWEKA)
 
+# Run tests
 test_check("RAutoWEKA")
--- tests/testthat/test-classifier.R
+++ tests/testthat/test-classifier.R
@@ -1,5 +1,69 @@
 context("test-classifier")
 
-testthat::test_that("autoWekaClassifier works", {
-  autoWekaClassifier(Species ~ ., iris, timeLimit = 1)
+testthat::test_that("RAutWEKA classifier works", {
+  data(iris)
+  require("mlbench")
+  data("HouseVotes84", package = "mlbench")
+  # # # # # # # # # # # # #
+  # Test autoWekaClassifier
+  # # # # # # # # # # # # #
+  formula = Species ~ .
+  data = iris
+  subset = NULL
+  naAction = na.omit
+  seed = 42
+  timeLimit = 1
+  memoryLimit = 1025
+  numberOfConfigs = 2
+  metric = "precision"
+  parallelRuns = 2
+  outputDebugInfo = TRUE
+
+  classifier <- autoWekaClassifier(formula, data, subset, naAction, seed, timeLimit, memoryLimit, numberOfConfigs, metric, parallelRuns, outputDebugInfo)
+
+  # Test parameters
+  testthat::expect_equal(classifier$awc$getSeed(), seed)
+  testthat::expect_equal(classifier$awc$getTimeLimit(), timeLimit)
+  testthat::expect_equal(classifier$awc$getMemLimit(), memoryLimit)
+  testthat::expect_equal(classifier$awc$getnBestConfigs(), numberOfConfigs)
+  testthat::expect_equal(classifier$awc$getMetric(), paste("Java-Object{", metric, "}"))
+  testthat::expect_equal(classifier$awc$getParallelRuns(), parallelRuns)
+  testthat::expect_equal(classifier$awc$getDebug(), outputDebugInfo)
+  testthat::expect_that(autoWekaClassifier(Class ~ ., HouseVotes84, na.action = na.fail), testthat::throws_error("missing values in object"))
+
+  # Test classifier
+  testthat::expect_is(classifier, "RAutoWEKAClassifier")
+  testthat::expect_true(metric %in% names(classifier))
+  testthat::expect_equal(classifier$levels, c("setosa", "versicolor", "virginica"))
+  testthat::expect_equal(ncol(classifier$mf), 5)
+  testthat::expect_equal(nrow(classifier$mf), 150)
+  testthat::expect_equal(nrow(classifier$numInstances), 150)
+  testthat::expect_equal(length(classifier$classPredicitions), 150)
+  testthat::expect_equal(levels(classifier$classPredicitions), classifier$levels)
+  testthat::expect_equal(ncol(classifier$probabilityPredictions), 3)
+  testthat::expect_equal(nrow(classifier$probabilityPredictions), 150)
+  testthat::expect_equal(colnames(classifier$probabilityPredictions), classifier$levels)
+  testthat::expect_equal(classifier$classPredicitions, predict(classifier, type = "class"))
+  testthat::expect_equal(classifier$probabilityPredictions, predict(classifier, type = "probability"))
+
+  # # # # # # # # # # # # #
+  # Test predict
+  # # # # # # # # # # # # #
+  classPredicitions = predict(classifier, iris[1:10,-5], type = c("class"))
+  testthat::expect_equal(length(classPredicitions), 10)
+  testthat::expect_equal(levels(classPredicitions), classifier$levels)
+  probabilityPredictions = predict(classifier, iris[1:10,-5], type = c("probability"))
+  testthat::expect_equal(ncol(probabilityPredictions), 3)
+  testthat::expect_equal(nrow(probabilityPredictions), 10)
+  testthat::expect_equal(colnames(probabilityPredictions), classifier$levels)
+
+  # # # # # # # # # # # # #
+  # Test model.frame
+  # # # # # # # # # # # # #
+  testthat::expect_equal(model.frame(classifier), classifier$mf)
+
+  # # # # # # # # # # # # #
+  # Test terms
+  # # # # # # # # # # # # #
+  testthat::expect_equal(terms(classifier), classifier$terms)
 })
